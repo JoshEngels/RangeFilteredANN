78d670c69c3ba1cb8014bdd019ca9210ad7217c0
Shangdi <ubuntu@shangdiy-small-2>
2024-01-22 15:50:30 -0500

    milvus experiment add recall computation

--- experiments/run_milvus.py
+++ experiments/run_milvus.py
@@ -13,15 +13,21 @@ from pymilvus import (
 )
 from tqdm import tqdm
 from math import sqrt 
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 
+THREADS = 16  # Adjust this to the desired number of parallel processes
+print("THREADS", THREADS)
+print("If you want to change the number of process used, OMP_NUM_THREADS in docker-compose.yml should also be changed.")
 # Ensure index_cache/postfiler_vamana exists
 os.makedirs("index_cache/postfilter_vamana", exist_ok=True)
 os.makedirs("results", exist_ok=True)
 
 TOP_K = 10
+# TODO: change to None
+DATA_SUBSET = 100
+QUERY_SUBSET = 100
 EXPERIMENT_FILTER_WIDTHS = [str(-i) for i in range(1)] # 17
-INDEX_TYPES = ["IVF_FLAT"]  # , "IVF_PQ", "IVF_SQ8", "HNSW", "SCANN", "DISKANN", "FLAT"
+INDEX_TYPES = ["HNSW"]  # "DISKANN", "IVF_PQ", "IVF_SQ8", "HNSW", "SCANN", "IVF_FLAT", "FLAT"
 
 dataset_folder = "/data/parap/storage/jae/new_filtered_ann_datasets"
 
@@ -33,7 +39,7 @@ def compute_recall(gt_neighbors, results, top_k):
         recall += len(gt.intersection(res)) / len(gt)
     return recall / len(gt_neighbors)  # average recall per query
 
-def get_index_params(index_type, n):
+def get_index_params(index_type, n, metric):
     params = {
         "FLAT": {},
         "IVF_FLAT": {"nlist": int(sqrt(n))},  # TODO: add more parameters
@@ -48,8 +54,8 @@ def get_index_params(index_type, n):
 
     # print(fmt.format("Start Creating index IVF_FLAT"))
     index = {
-        "index_type": "IVF_FLAT",
-        "metric_type": "L2",
+        "index_type": index_type,
+        "metric_type": metric,
         "params": params[index_type],
     }
     return index
@@ -96,19 +102,23 @@ for dataset_name in [
 
     print("Loading data.")
     data = np.load(os.path.join(dataset_folder, f"{dataset_name}.npy"))
+    if DATA_SUBSET is not None:
+        data = data[:DATA_SUBSET]
     num_entities, dim = data.shape
     print("data.shape, ", data.shape)
 
     print("Loading queries.")
     queries = np.load(os.path.join(dataset_folder, f"{dataset_name}_queries.npy"))
+    if QUERY_SUBSET is not None:
+        queries = queries[:QUERY_SUBSET]
     print("queries.shape, ", queries.shape)
-    # TODO: remove
-    queries = queries[:1000]
 
     print("Loading filter values.")
     filter_values = np.load(
         os.path.join(dataset_folder, f"{dataset_name}_filter-values.npy")
     )
+    if DATA_SUBSET is not None:
+        filter_values = filter_values[:DATA_SUBSET]
     print("filter.shape, ", filter_values.shape)
 
     metric = "IP" if "angular" in dataset_name else "L2"
@@ -139,8 +149,8 @@ for dataset_name in [
 
     # use different indices
     for index in INDEX_TYPES:
-        print("Building index ", index)
-        index_params = get_index_params(index, num_entities)
+        print("Building index: ", index)
+        index_params = get_index_params(index, num_entities, metric)
         search_params = get_query_params(index, num_entities)
         print("index_params: ", index_params)
         print("search_params: ", search_params)
@@ -166,9 +176,14 @@ for dataset_name in [
                     dataset_folder, f"{dataset_name}_queries_2pow{filter_width}_gt.npy"
                 )
             )
+            if QUERY_SUBSET is not None:
+                query_filter_ranges = query_filter_ranges[:QUERY_SUBSET]
+                query_gt = query_gt[:QUERY_SUBSET]
             print("query_filter_ranges.shape", query_filter_ranges.shape)
             print("query_gt.shape", query_gt.shape)
-            times = np.zeros(len(query_filter_ranges))
+            manager = Manager()
+            search_results = manager.list([None] * len(query_filter_ranges))
+            times = manager.list([None] * len(query_filter_ranges))
             # Perform the search
             def search_point(ith_point):
                 filter_start = query_filter_ranges[ith_point][0]
@@ -181,30 +196,51 @@ for dataset_name in [
                 )
                 end_time = time.time()
                 times[ith_point] = end_time - start_time
+
+                assert(len(result) == 1)
+                hits = result[0]
+                result_i = []
+                distance = {}
+                for hit in hits:
+                    result_i.append(hit.id) # , priority field: {hit.entity.get('priority')}
+                    distance[hit.id] = hit.distance
+                # sort results by distance
+                result_i.sort(key=lambda k: distance[k])
+                search_results[ith_point]= result_i
+
                 return times[ith_point]
 
-            num_processes = 4  # Adjust this to the desired number of parallel processes
-            
+            start_time = time.time()
             # Create a Pool of worker processes
-            with Pool(num_processes) as pool:
+            with Pool(THREADS) as pool:
                 # Use tqdm to track progress
                 for time_taken in tqdm(pool.imap_unordered(search_point, range(len(query_filter_ranges)))):
                     # times.append(time_taken)
                     pass
-                
-                # results = []
-                # for hits in result:
-                #     result_i = []
-                #     for hit in hits:
-                #         # possible to return less than TOP_K results
-                #         result_i.append(hit.id) # , priority field: {hit.entity.get('priority')}
-                #     results.append(result_i)
-                # TODO (shangdi): compute recall and write csv file
-            total_time = sum(times)
+            end_time = time.time()  
+
+            
+            total_time = end_time - start_time
+            # average_recall = sum(recalls) / len(recalls)
+            average_recall = compute_recall(query_gt, search_results, TOP_K)
+            print("average recall = {:.4f}".format(average_recall ))
             print("search latency = {:.4f}s".format(total_time ))
+            # TODO (shangdi): add params
+            run_results.append((
+                    f"milvus_{index}",
+                    average_recall,
+                    total_time,
+                ))
+
+        with open(output_file, "a") as f:
+            for name, recall, total_time in run_results:
+                f.write(
+                    f"{filter_width},{name},{recall},{total_time/len(queries)},{len(queries)/total_time},{THREADS}\n"
+                )
         # delete index
         points.release()
         points.drop_index()
+
 
     # remove points from database
     utility.drop_collection("points")
