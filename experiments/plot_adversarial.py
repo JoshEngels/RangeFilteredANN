151e5bc3bae5719a76a5770087e78f7fa5301a58
Josh Engels <josh.adam.engels@gmail.com>
2024-01-29 20:50:45 -0500

    Shameless repeating myself for adversarial plot

--- experiments/plot_adversarial.py
+++ experiments/plot_adversarial.py
@@ -5,6 +5,9 @@ import os
 from plot import pareto_front
 import glob
 
+
+BASELINE_METHODS = ["postfiltering", "prefiltering", "milvus", "msvbase"]
+
 cmap = plt.get_cmap("tab10")
 next_unused_cmap_index = 0
 cmap_colors = {}
@@ -22,10 +25,17 @@ def plot(dataset_name):
     grouped_data = df.groupby(["method"])
 
     for method, group in grouped_data:
+        method = method[0]
         if method not in cmap_colors:
             cmap_colors[method] = cmap(next_unused_cmap_index)
             next_unused_cmap_index += 1
         color = cmap_colors[method]
+
+        if method in BASELINE_METHODS:
+            method = "Baseline: " + method.capitalize()
+            marker = "o"
+        else:
+            marker = "x"
 
         sorted_group = group.sort_values(by="recall", ascending=False)
 
@@ -34,15 +44,65 @@ def plot(dataset_name):
             1.0 / np.array(sorted_group["average_time"]),
         )
 
-        if len(x) == 1:
-            plt.plot(x, y, label=method[0], markersize=20, marker="x", color=color)
-        else:
-            plt.plot(x, y, label=method[0], color=color)
+        plt.plot(
+            x, y, label=method, color=color, marker=marker, markersize=10, linewidth=1
+        )
+    ax = plt.gca()
+
+    alpha = 10
+
+    def fun(x):
+        return 1 - (1 - x) ** (1 / alpha)
+
+    def inv_fun(x):
+        return 1 - (1 - x) ** alpha
+
+    ax.set_xscale("function", functions=(fun, inv_fun))
+
+    from matplotlib import ticker
+
+    ax.xaxis.set_major_formatter(ticker.LogitFormatter())
+
+    ticks = [
+        0,
+        1 / 2,
+        1 - 1e-1,
+        1 - 1e-2,
+        1 - 1e-3,
+        1 - 1e-4,
+    ]
+
+    ax.set_xticks(ticks)
+    ax.set_xlim(-0.3, max(ticks))
+
+    ax.grid(visible=True, which="major", color="0.85", linestyle="-")
 
     plt.xlabel("Recall")
     plt.ylabel("Queries Per Second")
-    # plt.legend(loc="lower center", bbox_to_anchor=(0.5, 0.98), ncol=5)
-    plt.legend(loc="center left")
+
+    swaps = {
+        "Baseline: Msvbase": "Baseline: VBASE",
+        "three-split": "Three Split",
+        "vamana-tree": "DiskANN WST",
+        "super-postfiltering": "Super Postfiltering",
+        "optimized-postfiltering": "Optimized Postfiltering",
+    }
+
+    handles, labels = ax.get_legend_handles_labels()
+    order = [
+        "Baseline: Prefiltering",
+        "Baseline: Postfiltering",
+        "Baseline: Milvus",
+        "Baseline: Msvbase",
+        "vamana-tree",
+        "three-split",
+        "optimized-postfiltering",
+        "super-postfiltering",
+    ]
+    handles = [handles[labels.index(method)] for method in order]
+    labels = [swaps[label] if label in swaps else label for label in order]
+
+    plt.legend(handles, labels, loc="upper right")
 
     plt.title(f"Pareto Fronts on Adversarial Data")
 
